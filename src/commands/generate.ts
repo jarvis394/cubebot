5f14dc29cae8b0be8a347b29dda2f7a44538eaef
jarvis394 <vlad.ekushev@mail.ru>
2020-05-14 14:48:07 +0300

    Improve canvas heigh calculation Add ability to get an image from the replied message Fix code style (a bit)

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,15 +1,11 @@
-/**
- * Just a quick note: canvas rendering is a mess. JS devs are insane and mad.
- */
-
-import Command, { ExecOptions } from '../models/Command'
+import Command, { ExecOptions } from '@models/Command'
 import { PhotoAttachment } from 'vk-io'
 import { createCanvas, loadImage } from 'canvas'
 import getWrappedText from '@utils/getWrappedText'
 
 const helpMessage = [
   '\n\nЧтобы сгенерировать демотиватор, достаточно упомянуть бота, написать текст и прикрепить фотографию.',
-  'Подробную информацию можно посмотреть в группе: vk.com/cubebot'
+  'Подробную информацию можно посмотреть в статье: vk.com/@cubebot-how-to'
 ].join('\n')
 const noPhotoMessage = [
   'Прикрепи фотографию 😖',
@@ -20,43 +16,74 @@ const noTextMessage = [
   '\nПодстрочный текст можно написать если сделать перенос строки в сообщении'
 ].join('\n') + helpMessage
 
-const CANVAS_HEIGHT = 10000, 
-      CANVAS_WIDTH = 1000, 
-      PADDING = 100, 
-      PADDING_TOP = 75,
-      BORDER_PADDING = 14
+const CANVAS_HEIGHT = 10000,
+  CANVAS_WIDTH = 1000,
+  PADDING = 100,
+  PADDING_TOP = 80,
+  BORDER_PADDING = 14
+const space = 24
+const titleTextSize = 64
+const lowerTextSize = 32
+const textSpacing = 18
+const titleSpacing = textSpacing + titleTextSize
+const lowerTextSpacing = textSpacing / 1.7 + lowerTextSize
 
 export class GenerateCommand implements Command {
   name = 'generate'
   alias = ['g', 'gen', 'г', 'ген']
 
+  getImageFromMessage(update: ExecOptions['update']) {
+    let result: PhotoAttachment = null
+    if (update.hasAttachments()) {
+      result = update.attachments[0] as PhotoAttachment
+    } else if (update.hasReplyMessage && update.replyMessage.hasAttachments()) {
+      result = update.replyMessage.attachments[0] as PhotoAttachment
+    }
+    return result
+  }
+
   async exec({ update, args }: ExecOptions) {
-    const attachedImage = update.attachments[0] as PhotoAttachment
+    const attachedImage = this.getImageFromMessage(update)
     const unformattedText = args.join(' ')
     const text = unformattedText.replace('\\', '')
-    
+
     if (!attachedImage) return update.reply(noPhotoMessage)
     if (!text || text == '') return update.reply(noTextMessage)
-    
-    const titleText = text.split('\n')[0]
-    const lowerText = text.split('\n')[1]
+
     const canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
     const ctx = canvas.getContext('2d', { alpha: false })
+    const titleText = text.split('\n')[0]
+    const lowerText = text.split('\n')[1]
     const maxTextWidth = CANVAS_WIDTH - PADDING * 2
-    ctx.font = '64px Times New Roman'
-    const wrappedTitleLines = getWrappedText(ctx, titleText, maxTextWidth)
-    ctx.font = '32px Roboto'
-    const wrappedLowerLines = getWrappedText(ctx, lowerText || ' ', maxTextWidth)
-    const image = await loadImage(attachedImage.largePhoto)
+    const image = await loadImage(attachedImage.mediumPhoto)
     const imageWidth = CANVAS_WIDTH - PADDING * 2
     const imageHeight = image.height * imageWidth / image.width
-    const textSpace = PADDING_TOP / 2 + 64  // Adding text's size as a padding which is 64px
-    const spacing = 6 + 64
-    const textX = imageHeight + PADDING_TOP * 1.3 + BORDER_PADDING * 2
-    
+    const textStartingYPosition = imageHeight + PADDING_TOP * 1.3 + BORDER_PADDING * 2
+    const wrappedTitleLines = getWrappedText({
+      ctx,
+      text: titleText,
+      maxWidth: maxTextWidth,
+      ctxOptions: { font: '64px Times New Roman' }
+    })
+    const wrappedLowerLines = getWrappedText({
+      ctx,
+      text: lowerText || '',
+      maxWidth: maxTextWidth,
+      ctxOptions: { font: '32px Roboto' }
+    })
+
     // Set canvas height
-    canvas.height = imageHeight + PADDING_TOP * 2 + wrappedTitleLines.length * spacing + PADDING_TOP / 2 + (lowerText ? (PADDING_TOP / 2 + wrappedLowerLines.length * spacing / 2) : 0)
-    
+    const canavsComputedHeight =
+      PADDING_TOP * 2 + // Space on top and bottom
+      imageHeight + // Image height
+      BORDER_PADDING * 2 + // Space around border
+      space * 2 + // Space around title
+      (wrappedTitleLines.length * titleSpacing - titleSpacing + titleTextSize) + // Title height
+      (lowerText ? (
+        wrappedLowerLines.length * lowerTextSpacing - lowerTextSpacing + lowerTextSize
+      ) : -(space + titleSpacing - titleTextSize)) // Text height
+    canvas.height = canavsComputedHeight
+
     // A fix to disable anti-aliasing
     ctx.imageSmoothingEnabled = false
     ctx.translate(0.5, 0.5)
@@ -64,29 +91,29 @@ export class GenerateCommand implements Command {
     // Draw background in black
     ctx.fillStyle = 'black'
     ctx.fillRect(-0.5, -0.5, CANVAS_WIDTH, CANVAS_HEIGHT)
-    
+
     // Draw image
     ctx.drawImage(image, PADDING, PADDING_TOP, imageWidth, imageHeight)
-    
+
     // Draw border
     const borderPos = [
-      PADDING - BORDER_PADDING, 
+      PADDING - BORDER_PADDING,
       PADDING_TOP - BORDER_PADDING,
-      imageWidth + BORDER_PADDING * 2, 
+      imageWidth + BORDER_PADDING * 2,
       imageHeight + BORDER_PADDING * 2
     ]
     ctx.rect(borderPos[0], borderPos[1], borderPos[2], borderPos[3])
     ctx.strokeStyle = 'white'
     ctx.lineWidth = 4
     ctx.stroke()
-    
+
     // Draw upper text
     ctx.font = '64px Times New Roman'
     ctx.fillStyle = 'white'
     ctx.textAlign = 'center'
     ctx.textBaseline = 'top'
     wrappedTitleLines.forEach((e, i) => {
-      ctx.fillText(e, CANVAS_WIDTH / 2, textX + i * spacing)
+      ctx.fillText(e, CANVAS_WIDTH / 2, textStartingYPosition + i * titleSpacing)
     })
 
     // Draw upper text
@@ -94,11 +121,11 @@ export class GenerateCommand implements Command {
       ctx.font = '32px Roboto'
       ctx.textBaseline = 'top'
       wrappedLowerLines.forEach((e, i) => {
-        const y = textX + textSpace + (wrappedTitleLines.length - 1) * spacing + i * spacing / 2
+        const y = textStartingYPosition + space + wrappedTitleLines.length * titleSpacing + i * lowerTextSpacing
         ctx.fillText(e, CANVAS_WIDTH / 2, y)
       })
     }
-    
+
     return await update.sendPhotos(canvas.toBuffer())
   }
 }
--- src/utils/getWrappedText.ts
+++ src/utils/getWrappedText.ts
@@ -1,7 +1,19 @@
-const getLines = (ctx: CanvasRenderingContext2D, text: string, maxWidth: number) => {
+interface Options {
+  ctx: CanvasRenderingContext2D, 
+  text: string, 
+  maxWidth: number,
+  ctxOptions: Record<string, string | number>
+}
+
+const getLines = (options: Options) => {
+  const { ctx, text, maxWidth, ctxOptions } = options
   const words = text.split(' ')
   let lines = []
   let currentLine = words[0]
+
+  for (const param in ctxOptions) {
+    ctx[param] = ctxOptions[param]
+  }
 
   for (var i = 1; i < words.length; i++) {
     const word = words[i]
